1ac00194fcf18ba80f3bbb9e17475bd913ae4ef1
Jonathan <johnnydjohnson81@gmail.com>
2022-11-12 16:12:11 +0200

    changes in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,27 @@
 import Layout from "../components/Layout"; //the Layout component is imported to be rendered in _app.js
 import "../styles/globals.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-7Q05NMFFNZ`}
+      />
+
+      <Script strategy="afterInteractive">
+        {`  window.dataLayer = window.dataLayer || [];
+   function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-7Q05NMFFNZ');`}
+      </Script>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
